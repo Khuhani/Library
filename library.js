c3ff1a2bce26c94164c128e0e1ef8d31c4012bd1
Khuhani <suleimanmigwi@gmail.com>
2025-07-16 15:04:23 +0300

    The app can now populate the added books perfectly in a grid, with each grid styled appropriately.

--- library.js
+++ library.js
@@ -1,5 +1,5 @@
 //All books stored in an array
-const myLibrary = [];
+let myLibrary = [];
 
 //Constructor for creating books
 function Book(title,author,pages,status) {
@@ -20,8 +20,9 @@ const title = document.querySelector('#title').value;
 const author = document.querySelector('#author').value;
 const pages = document.querySelector('#pages').value;
 const readStatus = document.querySelector('#read').value;
+const id = crypto.randomUUID();
 
-const book = new Book(title,author,pages,readStatus);
+const book = new Book(title,author,pages,readStatus,id);
 
 //Add new book to library
 addBookToLibrary(book);
@@ -31,15 +32,16 @@ function addBookToLibrary(book) {
     myLibrary.push(book);
 
     //After pushing books to the library
-    displayBooks();
+    //displayBooks();
+    createCard();
     clearFields();
 };
 
-function displayBooks() {
+/*function displayBooks() {
     myLibrary.forEach(book => {
         createCard(book)
     });
-};
+};*/
 
 function clearFields() {
     document.querySelector('#title').value = ' ';
@@ -47,25 +49,42 @@ function clearFields() {
     document.querySelector('#pages').value = ' ';
 }
 
+const libSection = document.querySelector('.lib-section');
+
 function createCard(book) {
-    const card = document.querySelector('.card');
+    /*const card = document.querySelector('.card');
+    const cardContent = document.createElement('div');*/
+    myLibrary.forEach (book => {
     const cardContent = document.createElement('div');
+    cardContent.classList.add('card');
+    cardContent.setAttribute('data-id', book.id);
 
     if (book.status == 'Read') {
-        cardContent.textContent = `
-        ${book.title}
-        ${book.author}
-        ${book.pages}
-        Read
-        ${book.id}`
+        cardContent.innerHTML = `
+        <h3>${book.title}</h3>
+        <p><strong>Author:</strong>${book.author}</p>
+        <p><strong>Pages:</strong>${book.pages}</p>
+        <p><strong>Status:</strong>${book.status}</p>
+        <p><strong>ID:</strong>${book.id}</p>
+        <button class='delete-btn' onclick="deleteBook('${book.id}')">Delete</button>
+        `
     } else {
-        cardContent.textContent = `
-        ${book.title}
-        ${book.author}
-        ${book.pages}
-        Not Read
-        ${book.id}`
+        cardContent.innerHTML = `
+        <h3>${book.title}</h3>
+        <p><strong>Author:</strong>${book.author}</p>
+        <p><strong>Pages:</strong>${book.pages}</p>
+        <p><strong>Status:</strong>${book.status}</p>
+        <p><strong>ID:</strong>${book.id}</p>
+        <button class='.delete-btn' onclick="deleteBook('${book.id}')">Delete</button>
+        `
     }
 
-    card.appendChild(cardContent);
+    libSection.appendChild(cardContent);
+});
 } 
+
+//Deletes Books
+function deleteBook(id) {
+    myLibrary = myLibrary.filter(book => book.id !== id);
+    createCard();
+}
