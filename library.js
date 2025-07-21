d11f7e294b69687a7012da360672c628a57354f3
Khuhani <suleimanmigwi@gmail.com>
2025-07-21 10:56:08 +0300

    The delete button works now.All that was needed was adding a line of code that cleared the lib display prior to adding books.

--- library.js
+++ library.js
@@ -1,4 +1,4 @@
-//All books stored in an array
+//All books stored in this array
 let myLibrary = [];
 
 //Constructor for creating books
@@ -32,34 +32,28 @@ function addBookToLibrary(book) {
     myLibrary.push(book);
 
     //After pushing books to the library
-    //displayBooks();
     createCard();
     clearFields();
 };
-
-/*function displayBooks() {
-    myLibrary.forEach(book => {
-        createCard(book)
-    });
-};*/
 
 function clearFields() {
     document.querySelector('#title').value = ' ';
     document.querySelector('#author').value = ' ';
     document.querySelector('#pages').value = ' ';
-}
+};
 
 const libSection = document.querySelector('.lib-section');
 
-function createCard(book) {
-    /*const card = document.querySelector('.card');
-    const cardContent = document.createElement('div');*/
+function createCard() {
+    //clear current library display
+    libSection.innerHTML = ' ';
+
+    //create books 
     myLibrary.forEach (book => {
     const cardContent = document.createElement('div');
     cardContent.classList.add('card');
     cardContent.setAttribute('data-id', book.id);
 
-    if (book.status == 'Read') {
         cardContent.innerHTML = `
         <h3>${book.title}</h3>
         <p><strong>Author:</strong>${book.author}</p>
@@ -67,21 +61,11 @@ function createCard(book) {
         <p><strong>Status:</strong>${book.status}</p>
         <p><strong>ID:</strong>${book.id}</p>
         <button class='delete-btn' onclick="deleteBook('${book.id}')">Delete</button>
-        `
-    } else {
-        cardContent.innerHTML = `
-        <h3>${book.title}</h3>
-        <p><strong>Author:</strong>${book.author}</p>
-        <p><strong>Pages:</strong>${book.pages}</p>
-        <p><strong>Status:</strong>${book.status}</p>
-        <p><strong>ID:</strong>${book.id}</p>
-        <button class='.delete-btn' onclick="deleteBook('${book.id}')">Delete</button>
-        `
-    }
-
+        `;
+    
     libSection.appendChild(cardContent);
 });
-} 
+};
 
 //Deletes Books
 function deleteBook(id) {
